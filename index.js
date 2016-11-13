5e2eb79cd47a026636045870a80857dc069b1478
Ziga Cernigoj <zc6857@student.uni-lj.si>
2016-11-13 20:10:55 +0100

    primer s sliko

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-$.getJSON( "sets/example.json", function( data ) {
+$.getJSON("sets/example.json", function (data) {
     var set_id = data.set_id;
     var set_name = data.set_name;
     console.log(set_id + " " + set_name);
 
     var elements = data.elements;
 
-    for(var el in elements) {
+    for (var el in elements) {
         // console.log(el);
         console.log(elements[el]);
 
-        $("#kartice").append(
-            '<div class="col-lg-2">' +
+        if (elements[el].type === "text") {
+            $("#kartice").append(
+                '<div class="col-lg-2">' +
                 '<div class="panel panel-default">' +
-                    '<div class="panel-body">' +
-                        elements[el].text +
-                    '</div>' +
+                '<div class="panel-body">' +
+                elements[el].text +
                 '</div>' +
-            '</div>'
-        );
+                '</div>' +
+                '</div>'
+            );
+        }
+        else if (elements[el].type === "image") {
+            $("#kartice").append(
+                '<div class="col-lg-2">' +
+                '<div class="panel panel-default">' +
+                '<div class="panel-body">' +
+                '<img src="' + elements[el].image + '" class="img-responsive" alt="klavir">' +
+                '</div>' +
+                '</div>' +
+                '</div>'
+            );
+        }
 
 
     }
