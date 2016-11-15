7d091ec3311de1bcc99b49c69045d4a3dd41022b
zebond <matej.zebovec@gmail.com>
2016-11-15 23:23:46 +0100

    poračunana velikost kartic, in razporeditev.

--- matej_decoder.js
+++ matej_decoder.js
@@ -1,3 +1,4 @@
+var allCards;
 $.getJSON("sets/example_matej.json", function (data) {
 
     var pairs = data.pairs;
@@ -9,12 +10,11 @@ $.getJSON("sets/example_matej.json", function (data) {
     $.each(cards, function(index, card) {
         $("#kartice").append(get_card(card));
     });
+    allCards = cards.length;
 
 
 
 
-    var width = $(".kartica").width();
-    console.log(width);
 
 });
 
@@ -34,15 +34,61 @@ function get_card(el){
 }
 
 $( document ).ready(function() {
+    computeWidth(allCards);
     $(window).on('resize', function() {
-        computeWidth();
+        computeWidth(allCards);
     });
 });
 
-function computeWidth(){
-    //poračuna velikost kartic
-    var kWidth = $('body').width();
-    var kHeight = $('body').height();
-    console.log(kWidth + ' ' + kHeight);
-}
+/*
+nastavi velikost kartic in levi in desni odmik glavnega okna
+ */
+function computeWidth(n){
+    //nastavi padding na 0, da dobi pravilno velikost okna
+    $('#kartice').css({'padding-left': 0, 'padding-right': 0});
+    var kWidth = $('#kartice').width();
+    var kHeight = $('#kartice').height();
+    //število kartic v vrstici
+    var widthN = findDivisors(n, kWidth/kHeight);
+    //število kartic v stolpcu
+    var heightN = n/widthN;
+    //določi velikost tako, da optimalna razporeditev pade znotraj okna
+    //32 odštejemo, ker ima vsaka kartica 10px padding in 2px za border in še dodatnih 10 pri adnjem elementu
+    var size = Math.min(((kWidth-(widthN * 32 + 10))/widthN), ((kHeight-(heightN * 32 + 10))/heightN));
 
+    //odšteje margine in borderje in padding da dobimo levi in desni odmik, da so kartice poravnane na sredino
+    var paddingKartice = (kWidth-(widthN * 32 + 10)-(size*widthN))/2;
+    $('#kartice').css({'padding-left': paddingKartice, 'padding-right': paddingKartice});
+    $('.kartica').width(size);
+    $('.kartica').height(size);
+
+}
+/*
+parametra:  n - število kartic
+            bRatio - razmerje width/height diva, v katerega bomo dali kartice
+
+funkcija poišče vse deljitelje, najde optimalno razmerje razporeditve kartic
+
+vrne število kartic v eni vrstici
+ */
+function findDivisors(n, bRatio){
+    var nRatio = 0;
+    var bestW;
+    for(i=1; i<=n;i++){
+        var iRatio;
+        if((n % i) == 0){
+            iRatio = i / (n / i);
+            if (nRatio == 0) {
+                nRatio = iRatio;
+                bestW = i;
+            }
+            else {
+                if(Math.abs(bRatio-iRatio) < Math.abs(bRatio-nRatio)){
+                    nRatio = iRatio;
+                    bestW = i;
+                }
+            }
+        }
+    }
+    return bestW;
+}
